9d777d47e142db4e8d908d7eab920a1980e1e6be
sidhant-shr <shr.sidhant@gmail.com>
2023-10-26 11:48:27 +0530

    initial commit

---
+++ src/lend_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { lend_backend } from "../../declarations/lend_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await lend_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
